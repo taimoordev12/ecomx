40f79b95ecf2158ee0a1f46688824e1efaaff1f3
taimoor ashiq <a.taimoor12@gmail.com>
2020-06-04 01:58:17 +0500

    Updates

---
+++ precache-manifest.c49e24016ea71767c0610387f7494adc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b05d6a7fc0bc28bfb8db9e99e14d744",
+    "url": "/ecomx/index.html"
+  },
+  {
+    "revision": "97209c2727cdf8df861c",
+    "url": "/ecomx/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c45c1efb2e0a78a0cc72",
+    "url": "/ecomx/static/css/main.8ee7fc9a.chunk.css"
+  },
+  {
+    "revision": "97209c2727cdf8df861c",
+    "url": "/ecomx/static/js/2.df8ebb24.chunk.js"
+  },
+  {
+    "revision": "1f7e55e95101944e710d88e853fa36f6",
+    "url": "/ecomx/static/js/2.df8ebb24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c45c1efb2e0a78a0cc72",
+    "url": "/ecomx/static/js/main.67011942.chunk.js"
+  },
+  {
+    "revision": "da2af2a6b704bc42f9e3",
+    "url": "/ecomx/static/js/runtime-main.15854b35.js"
+  },
+  {
+    "revision": "4e3ed8a9cfdff13988e9b0b7f7cff453",
+    "url": "/ecomx/static/media/E2.4e3ed8a9.png"
+  },
+  {
+    "revision": "cf2715a44d3e0340c37250dcefea4376",
+    "url": "/ecomx/static/media/b2.cf2715a4.jpg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/ecomx/static/media/bag.30f07071.svg"
+  },
+  {
+    "revision": "33af8e4ceefdc9a61c883e56e91ed227",
+    "url": "/ecomx/static/media/e4.33af8e4c.jpg"
+  }
+]);
