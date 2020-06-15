fa2a401cd356c4a4ecc96f2aa36eb54e1c78d190
taimoor ashiq <a.taimoor12@gmail.com>
2020-06-16 01:39:59 +0500

    slider

--- src/Components/Slider/Slider.component.jsx
+++ src/Components/Slider/Slider.component.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import b2 from '../../assets/images/b2.jpg';
-import e4 from '../../assets/images/e4.jpg'
+import React,  { useState, useEffect } from 'react';
+import axios from 'axios';
 import {
   Carousel,
   CarouselItem,
@@ -8,38 +7,46 @@ import {
   CarouselIndicators,
   CarouselCaption
 } from 'reactstrap';
-console.log(e4);
-const items = [
-  {
-    src:{e4},
-    altText: 'Slide 1',
-    caption: 'Slide 1'
-  },
-  {
-    src: 'https://cdn.shopify.com/s/files/1/0100/5499/1969/t/7/assets/slideshow_4.jpg?v=8171324962076785025',
-    altText: 'Slide 2',
-    caption: 'Slide 2'
-  },
-  {
-    src: 'https://cdn.shopify.com/s/files/1/0100/5499/1969/t/7/assets/slideshow_3.jpg?v=15227626826394297397',
-    altText: 'Slide 3',
-    caption: 'Slide 3'
-  }
-];
+
+
 
 const Slider = (props) => {
+
+  const [BannerData, SetBannerData] = useState([]);
+
+  useEffect(() => {
+
+       
+      axios.get('/api/banners')
+      .then(function (res) {
+        // handle success
+        console.log(res.data);
+        SetBannerData(res.data);
+        console.log(BannerData.concat(res.data)); 
+        
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+    
+  },[]);
+
   const [activeIndex, setActiveIndex] = useState(0);
   const [animating, setAnimating] = useState(false);
 
   const next = () => {
     if (animating) return;
-    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    const nextIndex = activeIndex === BannerData.length - 1 ? 0 : activeIndex + 1;
     setActiveIndex(nextIndex);
   }
 
   const previous = () => {
     if (animating) return;
-    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    const nextIndex = activeIndex === 0 ? BannerData.length - 1 : activeIndex - 1;
     setActiveIndex(nextIndex);
   }
 
@@ -48,15 +55,14 @@ const Slider = (props) => {
     setActiveIndex(newIndex);
   }
 
-  const slides = items.map((item) => {
+  const slides = BannerData.map((item) => {
     return (
       <CarouselItem
         onExiting={() => setAnimating(true)}
         onExited={() => setAnimating(false)}
-        key={item.src}
+        key={item._id}
       >
-        <img src={item.src} className="img-fluid" alt={item.altText} />
-        <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+        <img src={item.imageData} className="img-fluid" alt={item.imageName} />
       </CarouselItem>
     );
   });
@@ -66,9 +72,10 @@ const Slider = (props) => {
       activeIndex={activeIndex}
       next={next}
       previous={previous}
-    >
-      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+    >  
+      <CarouselIndicators items={BannerData} activeIndex={activeIndex} onClickHandler={goToIndex} />
       {slides}
+      
       <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
       <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
     </Carousel>
